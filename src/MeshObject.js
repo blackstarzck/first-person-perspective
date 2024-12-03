27fa7a720495759acfc315e892d79b8ec2dd3c06
chanki <bucheongosok@gmail.com>
2024-12-03 15:27:09 +0900

    feat: Add physics(cannon-es)

--- src/MeshObject.js
+++ src/MeshObject.js
@@ -3,6 +3,13 @@ import {
   BoxGeometry,
   MeshLambertMaterial,
 } from 'three';
+import {
+  Vec3, // x, y, z
+  Box,
+  Body,
+  Quaternion
+ } from 'cannon-es';
+
 
 export class MeshObject {
   constructor(info) {
@@ -23,6 +30,11 @@ export class MeshObject {
     this.modelSrc = info.modelSrc;
     this.mapSrc = info.mapSrc;
     this.scene = info.scene;
+
+    // Cannon
+    this.mass = info.mass || 0;
+    this.cannonWorld = info.cannonWorld;
+    this.cannonMaterial = info.cannonMaterial;
     
     if(info.modelSrc){
       // GLB
@@ -33,14 +45,14 @@ export class MeshObject {
           console.log(`${this.name} loaded`);
           this.mesh.traverse((child) => {
             if(child.isMesh){
-              child.castShadow = true;
+              child.castShadow = true; // 복잡한 모델은 shadow를 받지 못할 수도 있음
             };
           });
-          this.mesh.castShadow = true; // 복잡한 모델은 shadow를 받지 못할 수도 있음
           this.mesh.position.set(this.x, this.y, this.z);
           this.mesh.rotation.set(this.rotationX, this.rotationY, this.rotationZ);
 
           this.scene.add(this.mesh);
+          this.setCannonBody();
         },
         (xhr) => {
           // console.log((xhr.loaded / xhr.total * 100) + '% loaded');
@@ -65,6 +77,7 @@ export class MeshObject {
           this.mesh.rotation.set(this.rotationX, this.rotationY, this.rotationZ);
 
           this.scene.add(this.mesh);
+          this.setCannonBody();
         },
       );
     }else{
@@ -81,6 +94,18 @@ export class MeshObject {
       this.mesh.rotation.set(this.rotationX, this.rotationY, this.rotationZ);
 
       this.scene.add(this.mesh);
+      this.setCannonBody();
     };
+  }
+
+  setCannonBody(){
+    this.cannonBody = new Body({
+      mass: this.mass,
+      position: new Vec3(this.x, this.y, this.z),
+      shape: new Box(new Vec3(this.width / 2, this.height / 2, this.depth / 2)),
+      material: this.cannonMaterial,
+    });
+
+    this.cannonWorld.addBody(this.cannonBody);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { MeshObject } from './MeshObject';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
+import * as CANNON from 'cannon-es';
 
 
 // Renderer
@@ -29,6 +31,7 @@ camera.position.set(0, 3, 7);
 scene.add(camera);
 
 // Controls
+const controls = new PointerLockControls(camera, document.body);
 const gltfLoader = new GLTFLoader();
 const textureLoader = new THREE.TextureLoader();
 
@@ -42,17 +45,41 @@ pointLight.position.set(0, 10, 0);
 
 scene.add(ambientLight, pointLight);
 
+// Physics
+const cannonWorld = new CANNON.World();
+cannonWorld.gravity.set(0, -9.82, 0);
+
+const defaultCannonMaterial = new CANNON.Material('default');
+const defaultContactMaterial = new CANNON.ContactMaterial(
+	defaultCannonMaterial,
+	defaultCannonMaterial,
+	{
+		friction: 1,
+		restitution: 0.2
+	}
+);
+
+cannonWorld.defaultContactMaterial = defaultContactMaterial;
+
+const cannonObjects = []; // 물리엔진에 영향을 받는 모든 객체를 넣어 제어
+
+
 // Mesh
 const ground = new MeshObject({
+	cannonWorld,
+	cannonMaterial: defaultCannonMaterial,
 	name: 'ground',
 	color: '#092E66',
 	width: 50,
 	height: 0.1,
 	depth: 50,
 	scene,
+	y: -0.05,
 	differenceY: 0
 });
 const floorMesh = new MeshObject({
+	cannonWorld,
+	cannonMaterial: defaultCannonMaterial,
 	name: 'floor',
 	width: 5,
 	height: 0.4,
@@ -63,6 +90,8 @@ const floorMesh = new MeshObject({
 });
 
 const wall1 = new MeshObject({
+	cannonWorld,
+	cannonMaterial: defaultCannonMaterial,
 	name: 'wall1',
 	width: 5,
 	height: 3,
@@ -70,7 +99,10 @@ const wall1 = new MeshObject({
 	z: -2.4,
 	scene
 });
+
 const wall2 = new MeshObject({
+	cannonWorld,
+	cannonMaterial: defaultCannonMaterial,
 	name: 'wall2',
 	width: 0.2,
 	height: 3,
@@ -81,6 +113,9 @@ const wall2 = new MeshObject({
 });
 
 const desk = new MeshObject({
+	cannonWorld,
+	cannonMaterial: defaultCannonMaterial,
+	mass: 20,
 	scene,
 	loader: gltfLoader,
 	name: 'desk',
@@ -89,10 +124,13 @@ const desk = new MeshObject({
 	height: 0.8,
 	depth: 0.75,
 	x: 1.2,
-	z: -1.9
+	z: -1.9,
 });
 
 const lamp = new MeshObject({
+	cannonWorld,
+	cannonMaterial: defaultCannonMaterial,
+	mass: 10,
 	scene,
 	loader: gltfLoader,
 	name: 'lamp',
@@ -104,6 +142,10 @@ const lamp = new MeshObject({
 });
 
 const roboticVaccum = new MeshObject({
+	cannonWorld,
+	cannonMaterial: defaultCannonMaterial,
+	mass: 0.5,
+	mass: 10,
 	scene,
 	loader: gltfLoader,
 	name: 'roboticVaccum',
@@ -115,6 +157,8 @@ const roboticVaccum = new MeshObject({
 });
 
 const magazine = new MeshObject({
+	cannonWorld,
+	cannonMaterial: defaultCannonMaterial,
 	scene,
 	loader: textureLoader,
 	name: 'magazine',
@@ -128,20 +172,76 @@ const magazine = new MeshObject({
 	rotationX: THREE.MathUtils.degToRad(52)
 });
 
+cannonObjects.push(ground, floorMesh, wall1, wall2, desk, lamp, roboticVaccum, magazine);
+
 // Draw
 const clock = new THREE.Clock();
-function draw() {
-  
+let delta;
+
+
+const draw = () => {
+	delta = clock.getDelta();
+
+	let cannonStepTime = 1 / 60;
+
+	if(delta < 0.01) cannonStepTime = 1 / 120;
+
+	cannonWorld.step(cannonStepTime, delta, 3);
+
+	for(const object of cannonObjects){
+		if(object.cannonBody){
+			object.mesh.position.copy(object.cannonBody.position);
+			object.mesh.quaternion.copy(object.cannonBody.quaternion);
+		};
+	};
+
+  rotateCamera();
+
 	renderer.render(scene, camera);
 	renderer.setAnimationLoop(draw);
 }
 
-draw();
-
-function setLayout() {
+const setLayout = () => {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+let movementX = 0, movementY = 0;
+
+const updateMovementValue = (event) => {
+	movementX = event.movementX * delta;
+	movementY = event.movementY * delta;
+}
+
+const euler = new THREE.Euler(0, 0, 0, 'YXZ');
+const minPolarAngle = 0;
+const maxPolarAngle = Math.PI; // 180
+const rotateCamera = () => {
+	euler.setFromQuaternion(camera.quaternion);
+	euler.y -= movementX;
+	euler.x -= movementY;
+	euler.x = Math.max(
+		Math.PI / 2 - maxPolarAngle,
+		Math.min(Math.PI / 2 - minPolarAngle, euler.x)
+	);
+
+	movementX -= movementX * 0.2;
+	movementY -= movementY * 0.2;
+	if(Math.abs(movementX) < 0.005) movementX = 0;
+	if(Math.abs(movementY) < 0.005) movementY = 0;
+
+	camera.quaternion.setFromEuler(euler);
+}
+
+const setMode = (mode) => {
+	document.body.dataset.mode = mode;
+
+	if(mode === 'game'){
+		document.addEventListener('mousemove', updateMovementValue);
+	}else if(mode === 'wesite'){
+		document.removeEventListener('mousemove', updateMovementValue);
+	};
 }
 
 // Events
@@ -152,5 +252,12 @@ document.addEventListener('click', () => {
 });
 
 document.addEventListener('pointerlockchange', () => {
-	document.body.dataset.mode = document.pointerLockElement === canvas ? 'game' : 'website';
+	if(document.pointerLockElement === canvas){
+		setMode('game');
+	}else{
+		setMode('website');
+	};
 });
+
+
+draw();
